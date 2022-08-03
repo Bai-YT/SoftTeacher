07939d2eb5b3a6e2429e4537ba2b5fb4ddb5bafc
yatong bai <yatong.bai@scale.com>
2022-08-03 05:58:14 +0000

    Working baseline

--- configs/baseline/faster_rcnn_r50_caffe_fpn_coco_full_720k.py
+++ configs/baseline/faster_rcnn_r50_caffe_fpn_coco_full_720k.py
@@ -1,14 +1,24 @@
 _base_ = "base.py"
+model = dict(
+    backbone=dict(
+        depth=50,
+        init_cfg=dict(checkpoint="open-mmlab://detectron2/resnet50_caffe"),
+    )
+)
 
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=8,
+    workers_per_gpu=7,
     train=dict(
-        ann_file="data/coco/annotations/instances_train2017.json",
-        img_prefix="data/coco/train2017/",
+        ann_file="/home/ubuntu/project/data/COCO/annotations/instances_train2017.json",
+        img_prefix="/home/ubuntu/project/data/COCO/train2017/",
+    ),
+    val=dict(
+        ann_file="/home/ubuntu/project/data/COCO/annotations/instances_val2017.json",
+        img_prefix="/home/ubuntu/project/data/COCO/val2017/",
     ),
 )
 
-optimizer = dict(lr=0.02)
-lr_config = dict(step=[120000 * 4, 160000 * 4])
+optimizer = dict(lr=0.0, weight_decay=1e-4)
+lr_config = dict(step=[120000 * 4, 170000 * 4])
 runner = dict(_delete_=True, type="IterBasedRunner", max_iters=180000 * 4)
